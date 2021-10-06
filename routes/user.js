f0e3c0f3df6abe53113ec21112734435f06de2f0
bibhasroy93 <butunroy@gmail.com>
2021-10-07 01:18:04 +0530

    1st Commit

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+const router=require("express").Router();
+
+
+
+module.exports=router
+
+
+// router.get("/usertest",(req,res)=>{
+//     res.send("User test is Successful");
+// });
+
+// router.post("/userposttest",(req,res)=>{
+//     const username=req.body.username;
+//     res.send("Your username is : " +username);
+// });
